d08da682e702dbf68a3217af75be392f7bc46a43
Akir Oussama <akir.oussama@gmail.com>
2025-02-14 01:50:47 +0100

    add fiche produite mfe

---
+++ fiche-produit/src/ProductDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <h1>Fiche Produit</h1>
+      <img src="https://via.placeholder.com/150" alt="Placeholder" />
+      <div>
+        <h2>Titre: Exemple de Film</h2>
+        <p>Année: 2023</p>
+        <p>Genre: Drame</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails; 
