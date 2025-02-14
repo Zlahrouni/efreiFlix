f807fc0ee4bf6913fef0100c932094e614b41d8e
Akir Oussama <akir.oussama@gmail.com>
2025-02-14 01:02:20 +0100

    add css loader

--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -47,6 +47,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.jsx?$/,
         loader: "babel-loader",
         exclude: /node_modules/,
